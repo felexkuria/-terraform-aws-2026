cc8d8dc779c887fea111b7d99796ab702e2e6e2e
Felex Kuria <engineerfelex@gmail.com>
2026-03-17 22:32:32 +0300

    feat: Update AWS instance AMI and resource name, and lock the AWS provider version.

--- day_3/main.tf
+++ day_3/main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 resource "aws_instance" "web_server" {
-    ami = "ami-0c55b159cbfafe1f0"
-    instance_type="t3.micro"
+  ami           = "ami-0ecb62995f68bb549"
+  instance_type = "t3.micro"
+  tags={Name="Web_Server"}
 }
