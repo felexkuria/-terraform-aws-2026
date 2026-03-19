47aeeaaa3ca0052f2b1f2e7fea361f9b736d4d06
Felex Kuria <engineerfelex@gmail.com>
2026-03-19 23:42:37 +0300

    feat: Parameterize AWS instance and security group configurations using new variables.

--- day_4/main.tf
+++ day_4/main.tf
@@ -1,26 +1,27 @@
 provider "aws" {
-  region="us-east-1"
+  region=var.region
 }
+
 resource "aws_instance" "web_server" {
 ami = data.aws_ami.ubuntu.id
-instance_type="t3.micro"
+instance_type=var.instance_type
 vpc_security_group_ids = [aws_security_group.web_server_sg.id]
-tags = {Name="web_server"}
+tags = {Name=var.name}
 user_data = <<-EOF
   #!/bin/bash
   echo "Hello World" > index.html
-  nohup busybox httpd -f -p 8080&
+  nohup busybox httpd -f -p ${var.port} &
 EOF
   user_data_replace_on_change = true
 }
 
 resource "aws_security_group" "web_server_sg" {
-    name = "web_server_sg"
+    name = var.sg_name
     ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        from_port =var.port
+        to_port = var.port
+        protocol = var.protocol
+        cidr_blocks = var.cidr_blocks
     }
 }
 data "aws_ami" "ubuntu" {
---
+++ day_4/variables.tf
@@ -0,0 +1,32 @@
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+}
+variable "name" {
+  type = string
+  default = "web_server"
+}
+variable "port" {
+    type = number
+    default = 8080
+  
+}
+variable "protocol" {
+    type = string
+    default = "tcp"
+  
+}
+variable "cidr_blocks" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+  
+}
+variable "region" {
+    type = string
+    default = "us-east-1"
+  
+}
+variable "sg_name" {
+    type = string
+    default = "web_server_sg" 
+}
