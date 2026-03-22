eff6d56b874061ff75a8c963d30ec22cdc11fd72
Felex Kuria <engineerfelex@gmail.com>
2026-03-22 11:53:13 +0300

    feat: Add Terraform outputs for EC2 instance details and lock AWS provider version.

---
+++ day_7/env/dev/output.tf
@@ -0,0 +1,17 @@
+output "aws_instance_public_ip" {
+  value = aws_instance.example.public_ip
+}
+output "aws_security_group_id" {
+  value = aws_security_group.example_sg.id
+}
+output "aws_instance_id" {
+  value = aws_instance.example.id
+  }
+
+
+
+
+
+
+
+
