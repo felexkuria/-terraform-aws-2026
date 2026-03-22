8489d560a1c865eaf6d71cb5037eccc0a7c52dec
Felex Kuria <engineerfelex@gmail.com>
2026-03-22 11:56:46 +0300

    feat: Add output for the AWS instance name.

--- day_7/env/dev/output.tf
+++ day_7/env/dev/output.tf
@@ -7,7 +7,10 @@ output "aws_security_group_id" {
 output "aws_instance_id" {
   value = aws_instance.example.id
   }
-
+output "aws_instance_name" {
+  value = aws_instance.example.tags.Name
+  }
+  
 
 
 
