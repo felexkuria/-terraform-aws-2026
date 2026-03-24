15ddd5224dcb9827de73501ced2742491ff7dfed
Felex Kuria <engineerfelex@gmail.com>
2026-03-24 08:23:36 +0300

    feat: Implement a Terraform module for a webserver cluster and deploy it to dev and production environments.

---
+++ day_9/live/dev/services/webserver-cluster/main.tf
@@ -0,0 +1,15 @@
+module "webserver_cluster" {
+  # THE SOURCE: Four steps back to get out of 'live/dev/services/webserver-cluster'
+  # and into the 'modules' directory.
+  source = "../../../../modules/services/webserver-cluster"
+
+  cluster_name  = "webservers-dev"
+  instance_type = "t3.micro"
+  min_size      = 2
+  max_size      = 4
+}
+
+# We must 're-wire' the output if we want to see it in our dev terminal
+output "alb_dns_name" {
+  value = module.webserver_cluster.alb_dns_name
+}
